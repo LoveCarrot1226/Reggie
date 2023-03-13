4c9504c26b2afda0b2fba1cd6e96bffe7f5dc92c
LoveCarrot1226 <1208745339@qq.com>
2023-03-13 10:36:45 +0800

    使用knife4j生成swagger的api文档

--- src/main/java/com/example/config/WedMvcConfig.java
+++ src/main/java/com/example/config/WedMvcConfig.java
@@ -1,16 +1,27 @@
 package com.example.config;
 
 import com.example.common.JacksonObjectMapper;
+import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.List;
 @Slf4j
 @Configuration
+@EnableSwagger2
+@EnableKnife4j
 public class WedMvcConfig implements WebMvcConfigurer {
     /**
      * 扩展Mvc框架消息转换器
@@ -28,5 +39,22 @@ public class WedMvcConfig implements WebMvcConfigurer {
         converters.add(0,messageConverter);//index为0，让自己设置的转换器优先被使用
     }
 
+    @Bean
+    public Docket createRestApi() {
+        // 文档类型
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.example.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
 
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("瑞吉外卖")
+                .version("1.0")
+                .description("瑞吉外卖接口文档")
+                .build();
+    }
 }
